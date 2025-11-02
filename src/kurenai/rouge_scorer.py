a9d1844013e01733e9c53b25ea1ef3f8491c143a
ftnext <takuyafjp+develop@gmail.com>
2025-11-02 11:38:38 +0900

    [docs] Add type hints

--- src/kurenai/rouge_scorer.py
+++ src/kurenai/rouge_scorer.py
@@ -1,19 +1,51 @@
 from __future__ import annotations
 
+from collections.abc import Iterable
+from typing import Literal, TypedDict
+
 from rouge_score.rouge_scorer import RougeScorer as OriginalRougeScorer
-from rouge_score.scoring import BaseScorer
+from rouge_score.scoring import BaseScorer, Score
 
 from kurenai.tokenizers import AllCharacterSupportTokenizer
 
+RougeType = Literal[
+    "rouge1",
+    "rouge2",
+    "rouge3",
+    "rouge4",
+    "rouge5",
+    "rouge6",
+    "rouge7",
+    "rouge8",
+    "rouge9",
+    "rougeL",
+    # "rougeLsum",  # TODO
+]
+
+
+class RougeScoreDict(TypedDict, total=False):
+    rouge1: Score
+    rouge2: Score
+    rouge3: Score
+    rouge4: Score
+    rouge5: Score
+    rouge6: Score
+    rouge7: Score
+    rouge8: Score
+    rouge9: Score
+    rougeL: Score
+
 
 class RougeScorer(BaseScorer):
-    def __init__(self, rouge_types: list[str]) -> None:
+    def __init__(self, rouge_types: Iterable[RougeType]) -> None:
         self._scorer = OriginalRougeScorer(
-            rouge_types, tokenizer=AllCharacterSupportTokenizer()
+            list(rouge_types), tokenizer=AllCharacterSupportTokenizer()
         )
 
-    def score(self, target, prediction):
+    def score(self, target: str, prediction: str) -> RougeScoreDict:
         return self._scorer.score(target, prediction)
 
-    def score_multi(self, targets, prediction):
+    def score_multi(
+        self, targets: Iterable[str], prediction: str
+    ) -> RougeScoreDict:
         return self._scorer.score_multi(targets, prediction)
