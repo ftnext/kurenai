0255ef378e01f169b0de61349f0f57bdd7174571
ftnext <takuyafjp+develop@gmail.com>
2025-11-04 20:21:18 +0900

    [docs] Add docstring referring to rouge-score library

--- src/kurenai/rouge_scorer.py
+++ src/kurenai/rouge_scorer.py
@@ -37,15 +37,53 @@ class RougeScoreDict(TypedDict, total=False):
 
 
 class RougeScorer(BaseScorer):
+    """Calculate rouges scores between two blobs of text.
+
+    Sample usage:
+        scorer = RougeScorer(["rouge1", "rougeL"])
+        scores = scorer.score("The quick brown fox jumps over the lazy dog",
+                              "The quick brown dog jumps on the log.")
+    """
+
     def __init__(self, rouge_types: Iterable[RougeType]) -> None:
+        """Initializes a new RougeScorer.
+
+        Valid rouge types that can be computed are:
+            rougeN (e.g. rouge1, rouge2, ..., rouge9): n-gram based scoring.
+            rougeL: Longest common subsequence based scoring.
+
+        Args:
+            rouge_types: An iterable of rouge types to calculate.
+        """
         self._scorer = OriginalRougeScorer(
             list(rouge_types), tokenizer=AllCharacterSupportTokenizer()
         )
 
     def score(self, target: str, prediction: str) -> RougeScoreDict:
+        """Calculates rouge scores between the target and prediction.
+
+        Args:
+            target: Ground truth text.
+            prediction: Predicted text.
+
+        Returns:
+            A dict mapping each rouge type to a Score object.
+        """
         return self._scorer.score(target, prediction)
 
     def score_multi(
         self, targets: Iterable[str], prediction: str
     ) -> RougeScoreDict:
+        """Calculates rouge scores between targets and prediction.
+
+        The target with the maximum f-measure is used for the final score for
+        each score type.
+
+        Args:
+            targets: An iterable of ground truth texts.
+            prediction: Predicted text.
+
+        Returns:
+            A dict mapping each rouge type to a Score object.
+        """
         return self._scorer.score_multi(targets, prediction)
